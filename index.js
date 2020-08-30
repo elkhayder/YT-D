ec2874938caa03bd970b700b8e37b0a2b9e3b2f9
elkhayder <zelkhayder@gmail.com>
2020-08-30 15:15:12 +0100

    Undo All MP4 changes

--- index.js
+++ index.js
@@ -33,35 +33,14 @@ app.get('/download/:url', async function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', "*");
     var videoUrl = decodeURIComponent(req.params.url);
     const VideoData = await SearchVideo(videoUrl, 1);
-    //res.header('Content-Disposition', 'attachment; filename="[YT-D.com] - ' + VideoData.items[0].title + '.mp4"'); // SET HEADERS
-    // Get audio and video stream going
-    const thisID = makeid(10);
-    const audio = ytdl(videoUrl, { filter: 'audioonly', quality: 'highestaudio' }).pipe(fs.createWriteStream(path.join("PreProcess", thisID + " - Audio.mp4")));
-    const video = ytdl(videoUrl, { filter: 'videoonly', quality: 'highestvideo' }).pipe(fs.createWriteStream(path.join("PreProcess", thisID + " - Video.mp4")));
-    const {audioR, videoR} = await Promise.all(audio, video);
-    // Start the ffmpeg child process
-    const ffmpegProcess = cp.spawn(ffmpeg, [
-      // Remove ffmpeg's console spamming
-      '-loglevel', '0', '-hide_banner',
-      // Pipe content
-      '-i', path.join("PreProcess", thisID + " - Audio.mp4"),
-      '-i', path.join("PreProcess", thisID + " - Video.mp4"),
-      // Choose some fancy codes
-      '-c:v', 'libx264', '-x265-params', 'log-level=0', '-crf', '28',
-      '-c:a', 'flac',
-      // Define output container
-      '-f', 'matroska', path.join("PreProcess", thisID + " - Final.mp4"),
-    ]);
-    ffmpegProcess.on('close', () => {
-      res.send(path.join("PreProcess", thisID + " - Final.mp4"));
-    });
-    /*const stream = ytdl(videoUrl, { quality: "highestaudio" });
+    res.header('Content-Disposition', 'attachment; filename="[MyField.tech] - ' + VideoData.items[0].title + '.mp3"'); // SET HEADERS
+    const stream = ytdl(videoUrl, { quality: "highestaudio" });
     stream.on('end', () => { // To be called at stream end event
       res.end(); // End response
       stream.destroy(); // destroy Stream leftovers
     });
 
-    stream.pipe(res);*/
+    stream.pipe(res);
 });
 
 app.get('*', function(req, res){
